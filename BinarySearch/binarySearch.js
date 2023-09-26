d8e3ad417f290cb8e78e3c38db9d253a267a9958
Jessy <martinezjessy393@gmail.com>
2023-09-25 22:10:15 -0700

    errors submitting

---
+++ BinarySearch/binarySearch.js
@@ -0,0 +1,40 @@
+//binary search game
+// 1. generate a random number between 1 and 100
+// 2. prompt the user to guess the number
+// 3. if the guess is correct, end the game
+// 4. if the guess is incorrect, tell the user if the guess was too high or too low
+// 5. let the user guess again
+// 6. repeat steps 2-5 until the user guesses the number
+// 7. when the game is over, tell the user how many guesses it took them to guess the number
+// 8. when the game is over, prompt the user to play again
+// 9. when the user says no, end the game
+// 10. when the game is over, tell the user how many games they played and their average number of guesses per game
+
+const theNumber = Math.floor(Math.random() * 100) + 1;
+
+const guessInput = document.getElementById(".guess");
+const submitButton = document.getElementById(".submitGuessButton");
+const message = document.getElementById(".result");
+
+let guessCount = 0;
+
+submitButton.addEventListener("click", function () {
+    const guess = Number(guessInput.value);
+
+    if (isNaN(guess) || guess < 1 || guess > 100) {
+        message.textContent = "Please enter a number between 1 and 100.";
+    } else {
+        guessCount++;
+        if (guess === theNumber) {
+            message.textContent = "Congratulations! You guessed the number!";
+            guessInput.disabled = true;
+            submitButton.disabled = true;
+        } else if (guess < theNumber) {
+            message.textContent = "Too low. Try again.";
+        } else if (guess > theNumber) {
+            message.textContent = "Too high. Try again.";
+        }
+
+
+    }
+});
