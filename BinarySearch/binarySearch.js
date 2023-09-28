46b76e30653f1cec37b60da8f29f69279b42fb8a
Jessy <martinezjessy393@gmail.com>
2023-09-27 23:57:36 -0700

    ensuring document gets loaded first

--- BinarySearch/binarySearch.js
+++ BinarySearch/binarySearch.js
@@ -10,31 +10,55 @@
 // 9. when the user says no, end the game
 // 10. when the game is over, tell the user how many games they played and their average number of guesses per game
 
-console.log("debuggin")
-const theNumber = Math.floor(Math.random() * 100) + 1;
+document.addEventListener("DOMContentLoaded", function () {
+    
+    console.log("debuggin")
+    const theNumber = Math.floor(Math.random() * 100) + 1;
 
-const guessInput = document.getElementById("guess");
-const submitButton = document.getElementById("submitGuessButton");
-const message = document.getElementById("result");
+    const guessInput = document.getElementById("guess");
+    const submitButton = document.getElementById("submitGuessButton");
+    const message = document.getElementById("result");
+    const restartButton = document.getElementById("playAgainButton");
 
-let guessCount = 0;
+    let guessCount = 0;
 
-submitButton.addEventListener("click", function () {
-    const guess = Number(guessInput.value);
-
-    console.log("debuggin1");
-    if (isNaN(guess) || guess < 1 || guess > 100) {
-        message.textContent = "Please enter a number between 1 and 100.";
-    } else {
-        guessCount++;
-        if (guess === theNumber) {
-            message.textContent = "Congratulations! You guessed the number!";
-            guessInput.disabled = true;
-            submitButton.disabled = true;
-        } else if (guess < theNumber) {
-            message.textContent = "Too low. Try again.";
-        } else if (guess > theNumber) {
-            message.textContent = "Too high. Try again.";
-        }
+    function initializeGame() {
+        guessCount = 0;
+        guessInput.disabled = false;
+        submitButton.disabled = false;
+        message.textContent = "";
+        guessInput.value = "";
+        guessInput.focus();
     }
+
+    submitButton.addEventListener("click", function () {
+        const guess = Number(guessInput.value);
+
+        console.log("debuggin1");
+        if (isNaN(guess) || guess < 1 || guess > 100) {
+            message.textContent = "Please enter a number between 1 and 100.";
+        } else {
+            guessCount++;
+            if (guess === theNumber) {
+                message.textContent = "Congratulations! You guessed the number!";
+                guessInput.disabled = true;
+                submitButton.disabled = true;
+            } else if (guess < theNumber) {
+                message.textContent = "Too low. Try again.";
+            } else if (guess > theNumber) {
+                message.textContent = "Too high. Try again.";
+            }
+        }
+
+        
+
+    });
+
+    restartButton.addEventListener("click", function () {
+        initializeGame();
+    });
+
+    initializeGame();
+
+    console.log("DOM fully loaded and parsed");
 });
